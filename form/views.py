802eeecf30fb5e263833d3792c8a88de7d024833
Ali <ali.ozgur@example.com>
2023-08-23 01:48:39 +0300

    added gmail function

--- form/views.py
+++ form/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
 from .models import FormData
 from .serializer import FormDataSerializers
 
-# Create your views here.
+from rest_framework.response import Response #mail için
+from django.core.mail import send_mail #mail için
+
+
 
 class FormDataMVS(ModelViewSet):
     queryset= FormData.objects.all()
@@ -12,3 +14,25 @@ class FormDataMVS(ModelViewSet):
 
 
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        # Form verilerini al
+        firstname = serializer.validated_data.get('firstname')
+        lastname = serializer.validated_data.get('lastname')
+        phonenumber = serializer.validated_data.get('phonenumber')
+        email = serializer.validated_data.get('email')
+        mesaj = serializer.validated_data.get('mesaj')
+
+        # E-posta gönderme işlemi
+        send_mail(
+            'Doruk Dijital Ajans Webden Gelen',
+            f'Ad: {firstname}\nSoyad: {lastname}\nTelefon: {phonenumber}\nE-posta: {email}\nMesaj: {mesaj}',
+            'your_email@gmail.com',  # Gönderici e-posta adresi
+            ['dorukdijitalajans@gmail.com'],  # Hedef e-posta adresi
+            fail_silently=False,
+        )
+
+       
--- main/settings.py
+++ main/settings.py
@@ -145,3 +145,14 @@ CORS_ALLOW_ALL_ORIGINS= True
 CORS_ALLOW_METHODS = [
     "POST",
 ]
+
+
+#E- MAİL AYARLARI
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dorukdijitalajans@gmail.com'
+EMAIL_HOST_PASSWORD = 'xswiowsapqspyqub' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
